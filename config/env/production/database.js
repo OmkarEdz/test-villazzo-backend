f2b277080fb429176be9d5dfde5cdef629b3eeb2
OmkarEdz <110821803+OmkarEdz@users.noreply.github.com>
2023-06-08 15:32:10 +0530

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -22,7 +22,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', '0.0.0.0'),
-      port: env.int('PGPORT', 5931),
+      port: env.int('PGPORT', 6582),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'password'),
