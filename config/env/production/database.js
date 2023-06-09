70a9d465d9a8a2d151dae5a80a6971ce1e6552fa
OmkarEdz <omkar.t@edreamz.in>
2023-06-09 20:33:11 +0530

    Added file for railway

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,32 +1,32 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: config.host,
-//       port: config.port,
-//       database: config.database,
-//       user: config.user,
-//       password: config.password,
-//       ssl: {
-//         rejectUnauthorized: false
-//       },
-//     },
-//     debug: false,
-//   },
-// });
-
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '0.0.0.0'),
-      port: env.int('PGPORT', 6582),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('PGHOST', '0.0.0.0'),
+//       port: env.int('PGPORT', 6582),
+//       database: env('PGDATABASE', 'strapi'),
+//       user: env('PGUSER', 'strapi'),
+//       password: env('PGPASSWORD', 'password'),
+//       ssl: env.bool(true),
+//     },
+//   },
+// });
